f555d977dfbdfef0dc659063f8ea9dbc76cf9331
Ryu Parish <ryuparish1115@gmail.com>
2020-11-09 00:07:15 -0600

    Got local Heroku to work, now the big boy

--- projecttakeoffapp/users/routes.py
+++ projecttakeoffapp/users/routes.py
@@ -2,7 +2,6 @@ from flask import render_template, url_for, flash, redirect, request, Blueprint
 from flask_login import login_user, current_user, logout_user, login_required
 from projecttakeoffapp import db, bcrypt
 from projecttakeoffapp.models import User, Post
-from werkzeug.security import check_password_hash, generate_password_hash
 from projecttakeoffapp.users.forms import RegistrationForm, LoginForm, UpdateAccountForm, RequestResetForm, ResetPasswordForm
 from projecttakeoffapp.users.utils import save_picture, send_reset_email
 
@@ -15,7 +14,7 @@ def register():
         return redirect(url_for('main.index'))
     form = RegistrationForm()
     if form.validate_on_submit():
-        hashed_password = generate_password_hash(form.password.data)
+        hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
         user = User(username=form.username.data, email=form.email.data, password=hashed_password)
         db.session.add(user)
         db.session.commit()
@@ -31,7 +30,7 @@ def login():
     form = LoginForm()
     if form.validate_on_submit():
         user = User.query.filter_by(email=form.email.data).first()
-        if user and check_password_hash(user.password, form.password.data):
+        if user and bcrypt.check_password_hash(user.password, form.password.data):
             login_user(user, remember=form.remember.data)
             next_page = request.args.get('next')
             return redirect(next_page) if next_page else redirect(url_for('posts.space'))
@@ -86,7 +85,7 @@ def reset_token(token):
         return redirect(url_for('users.reset_request'))
     form = ResetPasswordForm()
     if form.validate_on_submit():
-        hashed_password = generate_password_hash(form.password.data)
+        hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
         user.password = hashed_password
         db.session.commit()
         flash('Your password has been updated! You are now able to log in with your new password.', 'success')
