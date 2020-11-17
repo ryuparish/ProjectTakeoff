814ef8d7bcaa4e00b18bbcb16be06ec6059329e7
Ryu Parish <ryuparish1115@gmail.com>
2020-11-16 21:24:41 -0600

    Added reachable default picture

--- projecttakeoffapp/models.py
+++ projecttakeoffapp/models.py
@@ -14,7 +14,7 @@ class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    image_file = db.Column(db.String(120), nullable=False, default='http://projecttakeoffbucket.s3.amazonaws.com/default.png')
+    image_file = db.Column(db.String(120), nullable=False, default='http://projecttakeoffbucket.s3.amazonaws.com/default.jpg')
     password = db.Column(db.String(60), nullable=False)
     posts = db.relationship('Post', backref='author', lazy=True)
 
